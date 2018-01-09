3c6ea657c636b86ebf96b131cf73d9345f882d48
Alex Anzelm <anex.work@gmail.com>
2018-01-09 13:09:32 +0300

    ~ fix

--- src/uselect/components/uselect/uselect.component.ts
+++ src/uselect/components/uselect/uselect.component.ts
@@ -11,7 +11,7 @@ import {
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
-import _ from 'lodash';
+import * as _ from 'lodash';
 
 import { UselectDefaultConfig } from './../../classes/uselect-default-config.class';
 import {
